50413ce7c7905fe3bdcda45308c23dc28dccab5b
Andy Baker <andy@andybak.net>
2014-05-21 11:04:17 +0100

    fix deprecated mimetype warning

--- smart_selects/views.py
+++ smart_selects/views.py
@@ -10,7 +10,7 @@ def filterchain(request, app, model, field, value, manager=None):
     filter_queryset = get_filterchain_queryset(app, model, manager)
 
     rendered_choices = render_filterchain_choices(filter_queryset.filter(**filter_kwargs))
-    return HttpResponse(dumps(rendered_choices), mimetype='application/json')
+    return HttpResponse(dumps(rendered_choices), content_type='application/json')
 
 def filterchain_all(request, app, model, field, value, manager=None):
     filter_kwargs = get_filterchain_kwargs(field, value)
@@ -19,4 +19,4 @@ def filterchain_all(request, app, model, field, value, manager=None):
     rendered_choices = render_filterchain_choices(filter_queryset.filter(**filter_kwargs))
     rendered_choices.append({'value': "", 'display': "---------"})
     rendered_choices += render_filterchain_choices(filter_queryset.exclude(**filter_kwargs))
-    return HttpResponse(dumps(rendered_choices), mimetype='application/json')
+    return HttpResponse(dumps(rendered_choices), content_type='application/json')
